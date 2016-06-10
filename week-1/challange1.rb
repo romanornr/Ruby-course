dfac7b7cc6eafa44de675bbd212f9f98099c6f85
romanornr <romanornr@gmail.com>
2016-06-10 18:25:16 +0200

    removed not required array

--- week-1/challange1.rb
+++ week-1/challange1.rb
@@ -5,7 +5,6 @@ def calculate_age_by_seconds(seconds)
 		age = seconds / (365.25 * 60 * 60 * 24)
 		age_round = age.floor
 		months = ((age - age_round) * 12).floor
-		[age_round, months]
 		"You are #{age_round} years and #{months} months old"
 	end
 end
