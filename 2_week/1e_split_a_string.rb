5215d5382d82f4e07f5f649b1ba472678faf95ae
Victor Goff <keeperotphones@gmail.com>
2016-06-18 07:53:34 -0400

    Submission of split string with line number prefix
    Currently broken, but showing what it looks like and what it should look
    like.

--- 2_week/1e_split_a_string.rb
+++ 2_week/1e_split_a_string.rb
@@ -1,3 +1,22 @@
-s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n" 
-arr = s.split("\n")
-arr.each { |i| print "#{i} \n" }
+def line_split(line)
+  arr = line.split("\n")
+  arr.each { |i| print "#{i} \n" }
+end
+
+line = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+line_split(line)
+
+
+__END__
+
+The current run looks like this:
+
+Welcome to the forum. 
+Here you can learn Ruby. 
+Along with other members. 
+
+It should look something like this:
+
+Line 1: Welcome to the forum.
+Line 2: Here you can learn Ruby.
+Line 3: Along with other members.
