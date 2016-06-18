f52033610849bf05715fe148e2f95a957a680abd
Victor Goff <keeperotphones@gmail.com>
2016-06-18 09:13:11 -0400

    Splitting text in to lines with line prefix
    We split the text given on each new line.
    
    Map is creating a new array, for returning an array with the elements
    changed, and puts naturally outputs each element on a new line.
    
    Using with_index provides us with the option to give an offset, we use
    that with some string interpolation to prefis our lines with "line ##".

--- 2_week/1e_split_a_string.rb
+++ 2_week/1e_split_a_string.rb
@@ -1,9 +1,9 @@
-def line_split(line)
-	arr = line.split("\n")
-	count = 0
-	arr.each { |i| count+=1 
-		puts "Line #{count}: #{i}" }
+def split(text)
+  text.split("\n").map.with_index(1) do |line, index|
+    "Line #{index}: #{line}"
+  end
 end
 
-line = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n" 
-line_split(line)
+s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+puts split(s)
+
