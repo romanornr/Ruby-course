4a7692d2af0f24e318d535813cb8b47139504227
romanornr <romanornr@gmail.com>
2016-06-18 14:49:17 +0200

    week 1 added lines

--- 2_week/1e_split_a_string.rb
+++ 2_week/1e_split_a_string.rb
@@ -1,3 +1,9 @@
-s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n" 
-arr = s.split("\n")
-arr.each { |i| print "#{i} \n" }
+def line_split(line)
+	arr = line.split("\n")
+	count = 0
+	arr.each { |i| count+=1 
+		puts "Line #{count}: #{i}" }
+end
+
+line = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n" 
+line_split(line)
