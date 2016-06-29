a5deefd3fc62402a264e00d12564e1a3a3da574e
romanornr <romanornr@gmail.com>
2016-06-29 15:27:02 +0200

    recursion

---
+++ 3_week/6e_deaf_granny_part_1.rb
@@ -0,0 +1,15 @@
+def Deaf_Grandma()
+  puts "Say something to grandma: "
+  input = gets.chomp
+  unless input == "BYE"
+    if input != input.upcase
+      puts "HUH?! SPEAK UP, SONNY!"
+    else
+      puts "NO, NOT SINCE #{rand(1930...1950)} !"
+    end
+    Deaf_Grandma()
+  end
+  return
+end
+
+Deaf_Grandma()
