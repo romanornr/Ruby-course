ec4ed0f6d00383d5d36ed5e52dc853ece1e40d21
romanornr <romanornr@gmail.com>
2016-07-01 16:55:17 +0200

    prompt again when question was empty

--- 3_week/6e_deaf_granny_part_1.rb
+++ 3_week/6e_deaf_granny_part_1.rb
@@ -1,11 +1,11 @@
 def Deaf_Grandma()
   puts "Say something to grandma: "
   input = gets.chomp
-  unless input == "BYE" || input.empty?
+  unless input == "BYE"
     if input != input.upcase
-      puts "HUH?! SPEAK UP, SONNY!"
-    else
-      puts "NO, NOT SINCE #{rand(1930...1950)} !"
+       puts "HUH?! SPEAK UP, SONNY!"       
+    elsif !input.empty?
+       puts "NO, NOT SINCE #{rand(1930...1950)} !"
     end
     Deaf_Grandma()
   end
