01d823a697b7c2456632ee6ed7b083f00c8835c7
romanornr <romanornr@gmail.com>
2016-07-20 21:18:40 +0200

    Application guard added for promptable

--- 2_week/1c_prompt.rb
+++ 2_week/1c_prompt.rb
@@ -5,7 +5,9 @@ module Promptable
 	end
 end
 
+if __FILE__ == $PROGRAM_NAME
 include Promptable
 
 answer = Promptable.prompt('What operating system do you use: ')
 puts "You use #{answer} as an operating system"
+end
